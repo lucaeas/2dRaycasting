31b998f4d76ca6a877fd24b5b66328e1464c221a
lucaeas <zemarshzz@gmail.com>
2024-02-08 19:15:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,33 @@ while running:
             running = False
 
     mousePos = pygame.mouse.get_pos()
-
     red = 255
     green = 255
     blue = 255
+
     if mousePos != previousMousePos:
-        for largeur in range(0,800,40):
-            pygame.draw.line(screen, (0,0,0),previousMousePos, (largeur,800))
-            pygame.draw.line(screen, (0,0,0),previousMousePos, (largeur,0))
-            pygame.draw.line(screen, (red,green,blue),mousePos, (largeur,800))
-            pygame.draw.line(screen, (red,green,blue),mousePos, (largeur,0))
-        for hauteur in range(0,820,40):
-            pygame.draw.line(screen, (0,0,0),previousMousePos, (0,hauteur))
-            pygame.draw.line(screen, (0,0,0),previousMousePos, (800,hauteur))
-            pygame.draw.line(screen, (red,green,blue),mousePos, (0,hauteur))
-            pygame.draw.line(screen, (red,green,blue),mousePos, (800,hauteur))
+
+        for largeur in range(0,880,80):
+            pygame.draw.line(screen, (0,0,0), previousMousePos, (previousMousePos[0]+largeur,previousMousePos[1]+800), width=1)
+            pygame.draw.line(screen, (red,green,blue), mousePos, (mousePos[0]+largeur,mousePos[1]+800), width=1)
+            pygame.draw.line(screen, (0,0,0), previousMousePos, (previousMousePos[0]+largeur,previousMousePos[1]-800), width=1)
+            pygame.draw.line(screen, (red,green,blue), mousePos, (mousePos[0]+largeur,mousePos[1]-800), width=1)
+            pygame.draw.line(screen, (0,0,0), previousMousePos, (previousMousePos[0]-largeur,previousMousePos[1]+800), width=1)
+            pygame.draw.line(screen, (red,green,blue), mousePos, (mousePos[0]-largeur,mousePos[1]+800), width=1)
+            pygame.draw.line(screen, (0,0,0), previousMousePos, (previousMousePos[0]-largeur,previousMousePos[1]-800), width=1)
+            pygame.draw.line(screen, (red,green,blue), mousePos, (mousePos[0]-largeur,mousePos[1]-800), width=1)
+        
+        for hauteur in range(0,880,80):
+            pygame.draw.line(screen, (0,0,0), previousMousePos, (previousMousePos[0]+800,previousMousePos[1]+hauteur), width=1)
+            pygame.draw.line(screen, (red,green,blue), mousePos, (mousePos[0]+800,mousePos[1]+hauteur), width=1)
+            pygame.draw.line(screen, (0,0,0), previousMousePos, (previousMousePos[0]-800,previousMousePos[1]+hauteur), width=1)
+            pygame.draw.line(screen, (red,green,blue), mousePos, (mousePos[0]-800,mousePos[1]+hauteur), width=1)
+            pygame.draw.line(screen, (0,0,0), previousMousePos, (previousMousePos[0]+800,previousMousePos[1]-hauteur), width=1)
+            pygame.draw.line(screen, (red,green,blue), mousePos, (mousePos[0]+800,mousePos[1]-hauteur), width=1)
+            pygame.draw.line(screen, (0,0,0), previousMousePos, (previousMousePos[0]-800,previousMousePos[1]-hauteur), width=1)
+            pygame.draw.line(screen, (red,green,blue), mousePos, (mousePos[0]-800,mousePos[1]-hauteur), width=1)
+    
+    
     pygame.display.update()
     previousMousePos = mousePos
 
